7ede6b825880b4c0dd798d8fe08b4ce0a37209c6
Leorevoir <leo.quinzler@epitech.eu>
2024-11-07 18:30:22 +0100

    feat: remove all warnings

--- config/plug/statusline/lualine.nix
+++ config/plug/statusline/lualine.nix
@@ -4,135 +4,136 @@ let
 in
 {
   plugins.lualine = {
-    enable = true;
-    globalstatus = true;
-    disabledFiletypes = {
-      statusline = [
-        "dashboard"
-        "alpha"
-        "starter"
-      ];
-    };
-    theme = {
-      normal = {
-        a = {
-          bg = "#nil";
-        };
-        b = {
-          bg = "nil";
-        };
-        c = {
-          bg = "nil";
-        };
-        z = {
-          bg = "nil";
-        };
-        y = {
-          bg = "nil";
+    settings = {
+      enable = true;
+      globalstatus = true;
+      disabledFiletypes = {
+        statusline = [
+          "dashboard"
+          "alpha"
+          "starter"
+        ];
+      };
+      theme = {
+        normal = {
+          a = {
+            bg = "#nil";
+          };
+          b = {
+            bg = "nil";
+          };
+          c = {
+            bg = "nil";
+          };
+          z = {
+            bg = "nil";
+          };
+          y = {
+            bg = "nil";
+          };
         };
       };
-    };
-    inactiveSections = {
-      lualine_x = [
-        "filename"
-        "filetype"
-      ];
-    };
-    sections = {
-      lualine_a = [
-        {
-          mode = {
-            fmt = "string.lower";
-            color = {
-              fg = if config.colorschemes.base16.enable then colors.base04 else "nil";
-              bg = "nil";
-            };
-            separator.left = "";
-            separator.right = "";
-          };
-        }
-      ];
-      lualine_b = [
-        {
-          branch = {
-            icon = "";
-            color = {
-              fg = if config.colorschemes.base16.enable then colors.base04 else "nil";
-              bg = "nil";
-            };
-            separator.left = "";
-            separator.right = "";
-          };
-        }
-        {
-          diff = {
-            separator.left = "";
-            separator.right = "";
-          };
-        }
-      ];
-      lualine_c = [
-        {
-          diagnostic = {
-            extraConfig = {
-              symbols = {
-                error = " ";
-                warn = " ";
-                info = " ";
-                hint = "󰝶 ";
+      inactiveSections = {
+        lualine_x = [
+          "filename"
+          "filetype"
+        ];
+      };
+      sections = {
+        lualine_a = [
+          {
+            mode = {
+              fmt = "string.lower";
+              color = {
+                fg = if config.colorschemes.base16.enable then colors.base04 else "nil";
+                bg = "nil";
               };
+              separator.left = "";
+              separator.right = "";
             };
-            color = {
-              fg = if config.colorschemes.base16.enable then colors.base08 else "nil";
-              bg = "nil";
-            };
-            separator.left = "";
-            separator.right = "";
-          };
-        }
-      ];
-      lualine_x = [ "" ];
-      lualine_y = [
-        {
-          filetype = {
-            extraConfig = {
-              icon_only = true;
-            };
-            separator.left = "";
-            separator.right = "";
-          };
-        }
-        {
-          filename = {
-            extraConfig = {
-              symbols = {
-                modified = "";
-                readonly = "👁️";
-                unnamed = "";
+          }
+        ];
+        lualine_b = [
+          {
+            branch = {
+              icon = "";
+              color = {
+                fg = if config.colorschemes.base16.enable then colors.base04 else "nil";
+                bg = "nil";
               };
+              separator.left = "";
+              separator.right = "";
             };
-            color = {
-              fg = if config.colorschemes.base16.enable then colors.base05 else "nil";
-              bg = "nil";
+          }
+          {
+            diff = {
+              separator.left = "";
+              separator.right = "";
             };
-            separator.left = "";
-            separator.right = "";
-          };
-        }
-      ];
-      lualine_z = [
-        {
-          location = {
-            color = {
-              fg = if config.colorschemes.base16.enable then colors.base0B else "nil";
-              bg = "nil";
+          }
+        ];
+        lualine_c = [
+          {
+            diagnostic = {
+              extraConfig = {
+                symbols = {
+                  error = " ";
+                  warn = " ";
+                  info = " ";
+                  hint = "󰝶 ";
+                };
+              };
+              color = {
+                fg = if config.colorschemes.base16.enable then colors.base08 else "nil";
+                bg = "nil";
+              };
+              separator.left = "";
+              separator.right = "";
             };
-            separator.left = "";
-            separator.right = "";
-          };
-        }
-      ];
+          }
+        ];
+        lualine_x = [ "" ];
+        lualine_y = [
+          {
+            filetype = {
+              extraConfig = {
+                icon_only = true;
+              };
+              separator.left = "";
+              separator.right = "";
+            };
+          }
+          {
+            filename = {
+              extraConfig = {
+                symbols = {
+                  modified = "";
+                  readonly = "👁️";
+                  unnamed = "";
+                };
+              };
+              color = {
+                fg = if config.colorschemes.base16.enable then colors.base05 else "nil";
+                bg = "nil";
+              };
+              separator.left = "";
+              separator.right = "";
+            };
+          }
+        ];
+        lualine_z = [
+          {
+            location = {
+              color = {
+                fg = if config.colorschemes.base16.enable then colors.base0B else "nil";
+                bg = "nil";
+              };
+              separator.left = "";
+              separator.right = "";
+            };
+          }
+        ];
+      };
     };
   };
 }
-
