4ee35a86dd58747d5714b346ed0377659cd98c78
= <=>
2024-11-25 18:41:25 +0100

    feat: refactor plugin

--- config/default.nix
+++ config/default.nix
@@ -5,55 +5,7 @@
     ./keys.nix
     ./sets.nix
     ./highlight.nix
-
-    ./plug/colorscheme/web-devicons.nix
-    ./plug/colorscheme/biscuit.nix
-    ./plug/colorscheme/colorscheme.nix
-
-    ./plug/completion/cmp.nix
-    ./plug/completion/lspkind.nix
-    ./plug/completion/schemastore.nix
-
-    ./plug/git/gitlinker.nix
-    ./plug/git/gitsigns.nix
-    ./plug/git/lazygit.nix
-    ./plug/git/worktree.nix
-
-    ./plug/lsp/fidget.nix
-    ./plug/lsp/hlchunk.nix
-    ./plug/lsp/lsp.nix
-    ./plug/lsp/lspsaga.nix
-    ./plug/lsp/none-ls.nix
-    ./plug/lsp/trouble.nix
-
-    ./plug/snippets/luasnip.nix
-
-    ./plug/statusline/lualine.nix
-
-    ./plug/treesitter/treesitter-textobjects.nix
-    ./plug/treesitter/treesitter.nix
-
-    ./plug/ui/btw.nix
-    ./plug/ui/bufferline.nix
-    ./plug/ui/indent-blankline.nix
-    ./plug/ui/noice.nix
-    ./plug/ui/nvim-notify.nix
-    ./plug/ui/telescope.nix
-    ./plug/ui/nvim-tree.nix
-
-    ./plug/utils/comment.nix
-    ./plug/utils/comment-box.nix
-    ./plug/utils/hardtime.nix
-    ./plug/utils/illuminate.nix
-    ./plug/utils/markview.nix
-    ./plug/utils/mini.nix
-    ./plug/utils/nvim-autopairs.nix
-    ./plug/utils/oil.nix
-    ./plug/utils/ufo.nix
-    ./plug/utils/undotree.nix
-    ./plug/utils/whichkey.nix
-    ./plug/utils/yaml-companion.nix
-    ./plug/utils/neocord.nix
+    ./plug/default.nix
   ];
   config = {
     extraConfigLua = ''
---
+++ config/plug/default.nix
@@ -0,0 +1,53 @@
+{
+  imports = [
+
+    ./colorscheme/biscuit.nix
+    ./colorscheme/colorscheme.nix
+
+    ./completion/cmp.nix
+    ./completion/lspkind.nix
+    ./completion/schemastore.nix
+
+    ./git/gitlinker.nix
+    ./git/gitsigns.nix
+    ./git/lazygit.nix
+    ./git/worktree.nix
+
+    ./lsp/fidget.nix
+    ./lsp/hlchunk.nix
+    ./lsp/lsp.nix
+    ./lsp/lspsaga.nix
+    ./lsp/none-ls.nix
+    ./lsp/trouble.nix
+
+    ./snippets/luasnip.nix
+
+    ./statusline/lualine.nix
+
+    ./treesitter/treesitter-textobjects.nix
+    ./treesitter/treesitter.nix
+
+    ./ui/btw.nix
+    ./ui/bufferline.nix
+    ./ui/indent-blankline.nix
+    ./ui/noice.nix
+    ./ui/nvim-notify.nix
+    ./ui/telescope.nix
+    ./ui/nvim-tree.nix
+
+    ./utils/comment.nix
+    ./utils/comment-box.nix
+    ./utils/hardtime.nix
+    ./utils/illuminate.nix
+    ./utils/markview.nix
+    ./utils/mini.nix
+    ./utils/nvim-autopairs.nix
+    ./utils/oil.nix
+    ./utils/ufo.nix
+    ./utils/undotree.nix
+    ./utils/whichkey.nix
+    ./utils/yaml-companion.nix
+    ./utils/neocord.nix
+  ];
+
+}
