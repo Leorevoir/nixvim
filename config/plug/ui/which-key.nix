1f5cc9c58d1e3c342f6e04af0af7a921abdeceb3
Leorevoir <leo.quinzler@epitech.eu>
2024-11-27 13:18:53 +0100

    fix: move whichkey -> /ui/which-key and custom paths/icons!

---
+++ config/plug/ui/which-key.nix
@@ -0,0 +1,72 @@
+{
+  plugins.which-key = {
+    enable = true;
+
+    settings = {
+      spec = [
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "Git";
+          desc = "Git";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "<leader>r";
+          group = "Grep";
+          desc = "Grep";
+          icon = "󰈞";
+        }
+        {
+          __unkeyed-1 = "<leader>q";
+          desc = "Quit nixvim";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "<leader>u";
+          group = "UI";
+          desc = "UI";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "<leader>c";
+          group = "Code";
+          desc = "Code";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "<leader>s";
+          group = "Search";
+          desc = "Search";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "<leader>w";
+          group = "Windows";
+          desc = "Windows";
+          icon = "󱂬";
+        }
+        {
+          __unkeyed-1 = "<leader>f";
+          group = "File";
+          desc = "File";
+          icon = "󰈞";
+        }
+        {
+          __unkeyed-1 = "<leader>x";
+          desc = " Destroy";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "<leader>e";
+          desc = " Tree";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "<leader>:";
+          desc = " Command history";
+          icon = "";
+        }
+      ];
+    };
+  };
+}
