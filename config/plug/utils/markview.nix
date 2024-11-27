8db18aa3fee11587ebfc51c69ce6130b40207a53
Leorevoir <leo.quinzler@epitech.eu>
2024-11-27 09:56:12 +0100

    feat: keep Markview plugin up to date !

--- config/plug/utils/markview.nix
+++ config/plug/utils/markview.nix
@@ -3,12 +3,12 @@
   extraPlugins = with pkgs.vimUtils; [
     (buildVimPlugin rec {
       pname = "markview.nvim";
-      version = "1.0.0";
+      version = "24.0.0";
       src = pkgs.fetchFromGitHub {
         owner = "OXY2DEV";
         repo = "markview.nvim";
         rev = "refs/tags/v${version}";
-        hash = "sha256-mhRg/cszW/3oXdC1yvbpCeqWQA9WLW5FvcqGd/wBTnE=";
+        hash = "sha256-Bkwhg4RstOSRx+Jmjq5n2xjEkvyZ4Mx85lWn0YqRHxY=";
       };
     })
   ];
