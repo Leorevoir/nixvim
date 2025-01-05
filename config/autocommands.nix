75663a8ebe803e428817182f3846bbd95d348aa2
= <=>
2025-01-05 18:59:40 +0100

    fix: mini | not displayed in startup menu anymore !

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -62,10 +62,8 @@
       event = [ "FileType" ];
       pattern = [
         "help"
-        "Startup"
-        "startup"
+        "alpha"
         "neo-tree"
-        "Trouble"
         "trouble"
         "notify"
       ];
