e01e0bdaf4455e338d599c2e6870ba0df7badd41
= <=>
2025-01-05 17:00:02 +0100

    uncomment lsp

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -42,39 +42,39 @@
         pyright = {
           enable = true;
         };
-        # helm_ls = {
-        #   enable = true;
-        #   extraOptions = {
-        #     settings = {
-        #       "helm_ls" = {
-        #         yamlls = {
-        #           path = "${pkgs.yaml-language-server}/bin/yaml-language-server";
-        #         };
-        #       };
-        #     };
-        #   };
-        # };
-        # yamlls = {
-        #   enable = true;
-        #   extraOptions = {
-        #     settings = {
-        #       yaml = {
-        #         schemas = {
-        #           kubernetes = "'*.yaml";
-        #           "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
-        #           "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
-        #           "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
-        #           "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
-        #           "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
-        #           "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
-        #           "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
-        #           "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
-        #           "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
-        #         };
-        #       };
-        #     };
-        #   };
-        # };
+        helm_ls = {
+          enable = true;
+          extraOptions = {
+            settings = {
+              "helm_ls" = {
+                yamlls = {
+                  path = "${pkgs.yaml-language-server}/bin/yaml-language-server";
+                };
+              };
+            };
+          };
+        };
+        yamlls = {
+          enable = true;
+          extraOptions = {
+            settings = {
+              yaml = {
+                schemas = {
+                  kubernetes = "'*.yaml";
+                  "http://json.schemastore.org/github-workflow" = ".github/workflows/*";
+                  "http://json.schemastore.org/github-action" = ".github/action.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-stable-2.9" = "roles/tasks/*.{yml,yaml}";
+                  "http://json.schemastore.org/kustomization" = "kustomization.{yml,yaml}";
+                  "http://json.schemastore.org/ansible-playbook" = "*play*.{yml,yaml}";
+                  "http://json.schemastore.org/chart" = "Chart.{yml,yaml}";
+                  "https://json.schemastore.org/dependabot-v2" = ".github/dependabot.{yml,yaml}";
+                  "https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json" = "*docker-compose*.{yml,yaml}";
+                  "https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json" = "*flow*.{yml,yaml}";
+                };
+              };
+            };
+          };
+        };
       };
     };
   };
